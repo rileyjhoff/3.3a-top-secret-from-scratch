44e9186e043b831082fb7a8379dfb3fc7683009f
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-22 16:37:30 -0700

    write test for POST /api/v1/users/sessions which should log in a user

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,14 +2,28 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
-describe('backend-express-template routes', () => {
+const testUser = {
+  email: 'test@test.com',
+  password: '123456',
+};
+
+describe('users routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('POST /api/v1/users/sessions should log in a user', async () => {
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send(testUser);
+    const { email } = testUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(Number),
+      email,
+    });
   });
 
   afterAll(() => {
