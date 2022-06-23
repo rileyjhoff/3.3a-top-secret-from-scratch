8246f3f3febdaa59bd8160ef829a5835a5abd1c9
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-22 17:57:32 -0700

    write test for GET /api/v1/secrets route, which should return a list of secrets if authenticated and return an error if not authenticated

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -3,13 +3,35 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const testUser = {
+  email: 'test@test.com',
+  password: '123456',
+};
+
 describe('secrets routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET /api/v1/secrets should return a list of secrets', async () => {
+    const res1 = await request(app).post('/api/v1/users').send(testUser);
+
+    expect(res1.status).toEqual(200);
+    expect(res1.body).toEqual({
+      id: expect.any(Number),
+      email: testUser.email,
+    });
+
+    const res2 = await request(app)
+      .post('/api/v1/users/sessions')
+      .send(testUser);
+
+    expect(res2.status).toEqual(200);
+    expect(res2.body.message).toEqual('Signed in successfully');
+
+    const res3 = await request(app).get('/api/v1/secrets');
+
+    expect(res3.status).toEqual(200);
   });
 
   afterAll(() => {
